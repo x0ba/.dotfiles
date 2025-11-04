0c0682c02969cc0f17e65e8ce5f45d91e6a57cf2
Daniel Xu <64868985+x0ba@users.noreply.github.com>
2025-11-04 14:53:55 -0800

    init

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add one dark
+  { "olimorris/onedarkpro.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
